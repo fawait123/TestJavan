6fb6a9aace2f61767ef86c11c16b038655a29112
fawait626 <fawait626gmail.com>
2022-12-21 18:30:14 +0700

    setup ejs

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
+// initialize package
 const express = require("express");
 const app = express();
+// use dotenv to use file .env
 require("dotenv").config();
 
+// get variabel port from environment
 const PORT = process.env.PORT || 3000;
 
+// set view engine to ejs
+app.set("view engine", "ejs");
+
+// route root
 app.get("/", (req, res) => {
-  return res.send({ message: "oke" });
+  return res.render("pages/home/index");
 });
 
 // listen server
